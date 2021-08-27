d4b85f648b618d73ec24d027d6d0e20cfb0f5b7e
Aditya Kushwaha <kushwahaa862@gmail.com>
2021-08-27 12:19:12 +0530

    some changes

--- config/index.js
+++ config/index.js
@@ -2,9 +2,9 @@ const config = {};
 
 config.databaseMicroService = {
     type: 'tcp',
-    port: process.env.DB_MICROSERVICE_PORT,
-    host: 'localhost',
-    protocol: 'http',
+    port: process.env.PORT || process.env.DB_MICROSERVICE_PORT,
+    // host: 'localhost',
+    // protocol: 'http',
     timeout: 180000,
 };
 
@@ -15,3 +15,5 @@ config.dbDetails = {
 };
 
 module.exports = config;
+// pm2 start index.js --name dbConnector -o out.log -e err.log
+// pm2 start app.js --name webApp -o out.log -e err.log
